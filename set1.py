cd3b2fc2f4e2395f357aac2229f62061417f9c1f
Jayavardha <53034509+Jayavardha@users.noreply.github.com>
2019-07-18 13:27:12 +0530

    Create set1.py

---
+++ set1.py
@@ -0,0 +1,7 @@
+a=int(input("enter the value of a"))
+if(a<0):
+    print("the given number is negative number")
+elif(a==0):
+    print("the given number is zero")
+else:
+    print("the given numbber is positive number")
